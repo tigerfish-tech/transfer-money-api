71eacb026d3051ad6fcaf0ceb4d765c423e40d40
Andrey Suptelya <asuptelya@beeline.kz>
2019-09-20 20:18:50 +0600

    user dao test

--- src/main/java/com/fintech/Application.java
+++ src/main/java/com/fintech/Application.java
@@ -1,6 +1,8 @@
 package com.fintech;
 
+import com.fintech.dao.DbConnectionManager;
 import com.fintech.routers.Router;
+import com.zaxxer.hikari.HikariConfig;
 import io.undertow.Undertow;
 import org.jboss.logging.Logger;
 
@@ -9,6 +11,14 @@ public class Application {
   private static final Logger log = Logger.getLogger(Application.class);
 
   public static void main(String[] args) {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl("jdbc:h2:mem:transfers");
+    config.setUsername("sa");
+    config.setPassword("sa");
+
+    DbConnectionManager.setConfig(config);
+    DbConnectionManager.create();
+
     Undertow server = Undertow.builder()
         .addHttpListener(8080, "0.0.0.0")
         .setHandler(Router.getInstance().routingHandler())
